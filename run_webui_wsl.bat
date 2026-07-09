772110e4ae28b3b5a0bd1db0d12b7a8dcb60d4bf
hy <martinlord@foxmail.com>
2026-07-08 21:51:25 -0700

    feat: open WebUI when service is ready

--- run_webui_wsl.bat
+++ run_webui_wsl.bat
@@ -6,7 +6,10 @@ cd /d "%SystemRoot%"
 
 echo Starting IndexTTS WebUI in WSL...
 echo URL: http://localhost:7860
+echo The browser will open automatically when the service is ready.
 echo.
+
+start "" /b powershell.exe -NoProfile -WindowStyle Hidden -Command "$url='http://localhost:7860'; for ($i=0; $i -lt 600; $i++) { try { $response=Invoke-WebRequest -UseBasicParsing -Uri $url -TimeoutSec 2; if ($response.StatusCode -ge 200) { Start-Process $url; break } } catch {}; Start-Sleep -Seconds 1 }"
 
 wsl.exe -- bash -lc "cd '%WSL_PROJECT_DIR%' && exec ./run_webui.sh"
 set "EXIT_CODE=%ERRORLEVEL%"
